21cf85a3a711a7e3d2d359dc451e27a187b16f18
trans <transfire@gmail.com>
2016-11-22 12:26:44 -0500

    Better sequence spec.

--- spec/yaml_sequence_spec.cr
+++ spec/yaml_sequence_spec.cr
@@ -1,7 +1,7 @@
 require "./spec_helper"
 
 describe YAML::Sequence do
-  describe "new sequence" do
+  describe "#new" do
     it "can be initialized with no arguments" do
       n = YAML::Sequence.new
       n.value.size.should eq(0)
@@ -14,21 +14,19 @@ describe YAML::Sequence do
       n.nodes[0].should be_a(YAML::Scalar)
       n.node(0).should be_a(YAML::Scalar)
     end
-  end
-  describe "custom sequence" do
-    it "has correct tag" do
-      n = YAML::Sequence.new(["FOO"], "!foo")
-      n.tag.should eq("!foo")
-      n = YAML::Sequence.new(["BAR"], "tag:foo.org,2002:bar")
-      n.tag.should eq("tag:foo.org,2002:bar")
-    end
-    it "has correct value" do
-      n = YAML::Sequence.new(["FOO"], "!foo")
-      n.nodes[0].value.should eq("FOO")
+    it "creates a custom sequence" do
+      it "has correct tag" do
+        n = YAML::Sequence.new(["FOO"], "!foo")
+        n.tag.should eq("!foo")
+        n.nodes[0].value.should eq("FOO")
+
+        n = YAML::Sequence.new(["BAR"], "tag:foo.org,2002:bar")
+        n.tag.should eq("tag:foo.org,2002:bar")
+      end
     end
   end
 
-  describe "enumerablity" do
+  describe "#each" do
     it "can iterate over value" do
       n = YAML::Sequence.new([1,2,3])
       i = 0; s = ""
@@ -36,12 +34,18 @@ describe YAML::Sequence do
       i.should eq(3)
       s.should eq("123")
     end
+  end
+
+  describe "#each_node" do
     it "can iterate over its nodes" do
       n = YAML::Sequence.new([1,2,3])
       i = 0
       n.each_node{ |n| i += 1; n.should be_a(YAML::Node) }
       i.should eq(3)
     end
+  end
+
+  describe "#map" do
     it "can map over its values" do
       n = YAML::Sequence.new([1,2,3])
       a = n.map{ |x| x }
@@ -50,7 +54,7 @@ describe YAML::Sequence do
     end
   end
 
-  describe "equality" do
+  describe "#==" do
     it "is equal to another scalar with same value and tag (1)" do
       a = YAML::Sequence.new(["test"], "!foo")
       b = YAML::Sequence.new(["test"], "!foo")
@@ -61,6 +65,9 @@ describe YAML::Sequence do
       b = YAML::Sequence.new(["100"])
       a.should eq(b)
     end
+  end
+
+  describe "#hash" do
     it "has the same hash if equal" do
       a = YAML::Sequence.new(["100"])
       b = YAML::Sequence.new(["100"])
@@ -68,31 +75,37 @@ describe YAML::Sequence do
     end
   end
 
-  describe "tag" do
+  describe "#tag" do
     it "can have a blank tag" do
       n = YAML::Sequence.new([1,2,3], "")
       n.tag.should eq("")
     end
+  end
+
+  describe "#canonical_each" do
     it "will nonethless have the proper canonical tag" do
       n = YAML::Sequence.new([1,2,3], "")
       n.canonical_tag.should eq("tag:yaml.org,2002:seq")
     end
   end
 
-  describe "value" do
+  describe "#value" do
     it "has a value" do
       s = YAML::Sequence.new(["test"])
       a = YAML::Scalar.new("test")
       s.value.should eq([a])
     end
-    it "can append a node" do
+  end
+
+  describe "#<<" do
+    it "can merge a node" do
       n = YAML::Sequence.new(["test"])
       n.value << YAML::Scalar.new("append")
       n.nodes[-1].value.should eq("append")
     end
   end
 
-  describe "style" do
+  describe "#style" do
     it "has a style" do
       n = YAML::Sequence.new(["test"])
       n.style.should eq(LibYAML::SequenceStyle::ANY)
